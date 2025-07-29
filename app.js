16b31defc3ab1b28d815e011f2228987b1d6eb9c
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-07-29 10:16:04 -0300

    Adicionado arquivo js e iniciado testes de aprendizagem

---
+++ app.js
@@ -0,0 +1,11 @@
+/********************************************************************************************
+* Objetivo: iniciar os primeiros comandos em Node.JS
+* Autor: Kauan Lopes Pereira
+* Data: 29/07/2025
+* Versão: 1.0.0.0
+********************************************************************************************/
+
+/* Comentário em bloco */
+// Comentário em linha
+
+console.log ('Aula 01 de node.JS')
