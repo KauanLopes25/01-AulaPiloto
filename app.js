657a417f4b80fb2564c6baf975df92fec5c28c08
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-07-29 11:24:53 -0300

    Finalizado primeira aula de programação em node.js

--- app.js
+++ app.js
@@ -8,4 +8,64 @@
 /* Comentário em bloco */
 // Comentário em linha
 
+/********************************************************************************************
+********************************* COMANDOS UTILIZADOS ***************************************
+
+* Console.log - printa no console um valor de uma variavel ou texto
+* var - declaração de uma variavel
+* typeof(nome da variavel) - retorna o tipo da variavel em string
+* require('Nome do arquivo') - Método para fazer importação de um arquivo como uma bibliotéca
+* toUpperCase - Altera o valor de uma string para maiusculo
+
+************************ FUNÇÕES UTILIZADAS NA BIBLIOTECA READLINE **************************
+* createInterface - cria um objeto da classe readline
+* question - Printa na tema uma string e precisa de uma função para retornar o valor digitado
+pelo usuário
+* close - fecha a interface de entrada de dados do readline
+
+********************************************************************************************/
+
+// Importação de biblioteca
+var readline = require('readline')
+
+// Criando um objeto de entradaDeDados para captar ou printar dados via terminal
+var entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+// Ativação da interface de leitura do console
+entradaDeDados.question('Digite seu nome: ', function(nome){
+    var nomeUsuario = nome
+
+    // Condição para tratativa de dados
+    if(nomeUsuario == ''){
+        console.log('Não foi possivel processar a solicitação')
+    }else {
+        console.log(`O nome digitado foi: ${String(nomeUsuario).toUpperCase}`)
+    }
+
+    entradaDeDados.close()
+})
+
+
+
+/* 
+
 console.log ('Aula 01 de node.JS')
+
+// Declarando de uma variavel
+var nome = 'Kauan'
+
+// Exemplo de concatenação de dados (simbolo de (+))
+console.log('O nome digitado foi: ' + nome)
+
+// Exemplo de concatenação de metodo reduzido e mais utilizado no mercado
+console.log(`O nome digitado foi: ${nome}`)
+
+console.log(typeof(nome))
+
+//Testando typeof
+var tipo = typeof(nome)
+console.log(`O tipo da variavel digitada foi: ${tipo}`)
+
+*/
